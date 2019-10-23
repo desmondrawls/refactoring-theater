eed3ec754835da3fe50e85ac2b106e7795427ad1
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2019-10-23 09:11:08 -0700

    Use promises instead of callbacks in main.

--- src/theatrical-players/index.ts
+++ src/theatrical-players/index.ts
@@ -1,22 +1,15 @@
 const fs = require('fs')
+const util = require('util')
 import { statement } from "./statement";
 
-const main = () => {
-  fs.readFile(`${__dirname}/plays.json`, (err, data) => {
-    if (err) {
-        throw err;
-    }
-    const parsedPlays = JSON.parse(data)
-    console.log('raw plays: ', parsedPlays);
-    fs.readFile(`${__dirname}/invoices.json`, (err, data) => {
-      if (err) {
-          throw err;
-      }
-      const parsedInvoices = JSON.parse(data)
-      console.log('raw invoices: ', parsedInvoices);
-      console.log(statement(parsedInvoices[0], parsedPlays))
-  });
-})
+const main = async () => {
+  try {
+    const plays = await util.promisify(fs.readFile)(`${__dirname}/plays.json`) 
+    const invoices = await util.promisify(fs.readFile)(`${__dirname}/invoices.json`) 
+    console.log(statement(JSON.parse(invoices)[0], JSON.parse(plays)))
+  } catch (error) {
+    console.log('error: ', error)
+  }
 }
 
 main()
