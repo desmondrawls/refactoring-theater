b680a38897cc6d38e029f20f6b1e260ccf02c85b
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2019-10-24 00:35:00 -0700

    Replace mutation with composition.

--- src/theatrical-players/index.ts
+++ src/theatrical-players/index.ts
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const util = require('util')
 import { statement } from "./statement";
-import { access } from "fs";
 
 const main = async () => {
   try {
--- src/theatrical-players/statement.ts
+++ src/theatrical-players/statement.ts
@@ -1,17 +1,21 @@
-export const statement = (invoice) => {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+export const statement = ({customer, performances}) => {
+  const greeting = `Statement for ${customer}\n`;
+  const total = performances.slice(1).reduce(addPerformance, performanceStatement(performances[0]))
+  const damage = `Amount owed is ${format(total.amount/100)}\n`
+  const salvage = `You earned ${total.volumeCredits} credits\n`
+  return greeting + total.statement + damage + salvage;
+}
 
-  for(let performance of invoice.performances) {
-      const currentStatement = performanceStatement(performance)
-      result += currentStatement.statement
-      totalAmount += currentStatement.amount
-      volumeCredits += currentStatement.volumeCredits
-  }
-  result += `Amount owed is ${format(totalAmount/100)}\n`
-  result += `You earned ${volumeCredits} credits\n`
-  return result;
+const addPerformance = (performances, performance) => addMerge(performances, performanceStatement(performance))
+
+const addMerge = (first, second) =>
+  Object.keys(first).reduce((acc, next) => ({...acc, [next]: first[next] + second[next]}), {})
+
+const performanceStatement = ({playName, basePrice, volumeCreditsRatio, audience: {total, premium, overflow, overflowPremium, overflowPenalty}}) => {
+  const amount = basePrice + overflowPenalty + overflowPremium * overflow + premium * total
+  const volumeCredits = Math.floor(total - 30) + Math.floor(total * volumeCreditsRatio)
+  const statement = ` ${playName}: ${format(amount/100)} (${total} seats)\n`
+  return { statement, amount, volumeCredits }
 }
 
 export const format = new Intl.NumberFormat("en-US", {
@@ -19,15 +23,4 @@ export const format = new Intl.NumberFormat("en-US", {
   currency: "USD", 
   minimumFractionDigits: 2
 }).format;
-
-const performanceStatement = ({playName, basePrice, volumeCreditsRatio, audience: {total, premium, overflow, overflowPremium, overflowPenalty}}) => {
-  let amount = basePrice + overflowPenalty + overflowPremium * overflow + premium * total;
-  let volumeCredits = 0;
-  volumeCredits += Math.floor(total - 30) + Math.floor(total * volumeCreditsRatio);
-  return {
-    statement: ` ${playName}: ${format(amount/100)} (${total} seats)\n`,
-    amount: amount,
-    volumeCredits: volumeCredits,
-  }
-}
 
